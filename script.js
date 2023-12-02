967f7ff7338907b30c50890a9b43294674a3b46b
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-12-02 19:41:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function updateTotal() {
+      const prices = document.querySelectorAll('.prices');
+      let totalPrice = 0;
+
+      prices.forEach(priceElement => {
+        totalPrice += parseFloat(priceElement.textContent);
+      });
+
+      const table = document.getElementById('groceryTable');
+      const totalRow = document.createElement('tr');
+      const totalCell = document.createElement('td');
+
+      totalCell.textContent = 'Total Price: $' + totalPrice.toFixed(2);
+      totalCell.setAttribute('colspan', '2');
+      totalRow.appendChild(totalCell);
+      
+      // Remove the previous total row if it exists
+      const existingTotalRow = table.querySelector('.total-row');
+      if (existingTotalRow) {
+        table.querySelector('tbody').removeChild(existingTotalRow);
+      }
+
+      totalRow.classList.add('total-row');
+      table.querySelector('tbody').appendChild(totalRow);
+    }
+
+    // Call the updateTotal function initially and whenever prices change
+    updateTotal();
